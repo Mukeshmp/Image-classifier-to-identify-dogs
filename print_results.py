bebd8d8b00b1294f5ec623b12d552bce3a1277b2
Mukeshmp <mukeshpaul5555@gmail.com>
2025-10-06 19:18:35 +0530

    Final Results

--- print_results.py
+++ print_results.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/print_results.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:
-# REVISED DATE: 
+# PROGRAMMER: Mukesh Paul
+# DATE CREATED: 06-10-2025
+# REVISED DATE: 06-10-2025
 # PURPOSE: Create a function print_results that prints the results statistics
 #          from the results statistics dictionary (results_stats_dic). It 
 #          should also allow the user to be able to print out cases of misclassified
